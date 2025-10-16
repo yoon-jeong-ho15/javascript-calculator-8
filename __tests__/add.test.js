15d7ed69a0d8397f2897233e200530f3d5980eba
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-10-16 15:50:56 +0900

    feat : implement custom separator handling
    - cehckSeparator : check if separator used input is not in FORBIDDEN
    - replaceSeparator : replaces all separators to ","

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -1,7 +1,35 @@
-import { removeSpace } from "../src/add.js";
+import { removeSpace, replaceSeparator } from "../src/add.js";
 
 test("removeSpace 테스트", () => {
   const input = "1 ,  2, 3";
   const result = removeSpace(input);
   expect(result).toBe("1,2,3");
+});
+
+describe("replaceSeparator", () => {
+  test("';})'가 모두 ','로 변환", () => {
+    const input = "//;})\n1,2;3}4)5";
+    const result = replaceSeparator(input);
+    expect(result).toBe("1,2,3,4,5");
+  });
+
+  test("';})'가 모두 ','로 변환", () => {
+    const input = "//$&\n1,2$3:4&5";
+    const result = replaceSeparator(input);
+    expect(result).toBe("1,2,3,4,5");
+  });
+
+  test("지정하지 않은 커스텀 구분자 사용", () => {
+    const input = "//;}$%\n1$2;3&4";
+    expect(() => replaceSeparator(input)).toThrow(
+      "[ERROR] % 는 커스텀 구분자로 사용할 수 없습니다."
+    );
+  });
+
+  test("금지된 구분자 사용 에러", () => {
+    const input = "//;}$%\n1$2;3%4";
+    expect(() => replaceSeparator(input)).toThrow(
+      "[ERROR] % 는 커스텀 구분자로 사용할 수 없습니다."
+    );
+  });
 });
--- src/add.js
+++ src/add.js
@@ -9,10 +9,32 @@ export function getResult(input) {
 
 export function validateInput(input) {
   input = removeSpace(input);
+  if (input.startsWith("//")) input = replaceSeparator(input);
+  input = input.replaceAll(":", ",");
+  input = input.split(",");
 
   return input;
 }
 
 export function removeSpace(input) {
   return input.replaceAll(" ", "");
+}
+
+export function replaceSeparator(input) {
+  const [sep, str] = input.substr(2).split("\n");
+  const separator = checkSeparator(sep);
+  const regex = new RegExp(`[${separator}]`, "g");
+  console.log("-----regex : ", regex);
+  console.log("-----str : ", str);
+  const result = str.replaceAll(regex, ",");
+
+  return result;
+}
+
+export function checkSeparator(separator) {
+  for (const ch of separator) {
+    if (FORBIDDEN.includes(ch))
+      throw new Error(`[ERROR] ${ch} 는 커스텀 구분자로 사용할 수 없습니다.`);
+  }
+  return separator + ":";
 }
