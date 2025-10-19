76a0e440de100a30f5e32e0c6f6d2b5c4e881746
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-10-19 22:43:07 +0900

    refactor : rename functions &  restructure function dependencies
    - 이름 변경한 함수들 :
      - replaceSeparator -> replaceSperatorsWithComma
      - getSeparator -> extractSeparators
      - validateSeparator -> checkIfForbidden
      - checkSeparator -> validateInput
    - 함수 호출(의존)관계 수정 :
     - validateInput 함수를 삭제하고 getResult에서 직접 호출
     - 문자열을 숫자 배열로 바꾸는 함수 getNumArr 추가, getResult에서 호출.

--- src/add.js
+++ src/add.js
@@ -1,16 +1,65 @@
 const FORBIDDEN = [".", "/", "*", "-", "^", "%", "+"];
 
 export function getResult(input) {
-  const numbers = validateInput(input);
+  input = removeSpace(input);
+  input = replaceSeparatorsWithComma(input);
+
+  // 지정하지 않은 커스텀 구분자를 사용했거나
+  // "//"와 "\\n" 대신
+  validateInput(input);
+
+  const numbers = getNumArr(input);
+
   let result = 0;
   numbers.map((num) => (result += num));
   return result;
 }
 
-export function validateInput(input) {
-  input = removeSpace(input);
-  input = replaceSeparator(input);
+function removeSpace(input) {
+  return input.replaceAll(" ", "");
+}
 
+function replaceSeparatorsWithComma(input) {
+  let separator = ":";
+  const isHavingCustom = input.includes("\\n");
+  if (isHavingCustom) {
+    let [separatorPart, stringPart] = input.split("\\n");
+    separator += extractSeparators(separatorPart);
+    input = stringPart;
+  }
+  const regex = new RegExp(`[${separator}]`, "g");
+  input = input.replaceAll(regex, ",");
+  return input;
+}
+
+function extractSeparators(seperator) {
+  if (!seperator.startsWith("//")) {
+    throw new Error(
+      `[ERROR] 커스텀 구분자를 사용하려면 앞에 "//"를 입력해주세요`
+    );
+  }
+  separator = seperator.substr(2);
+  checkifForbidden(separator);
+  return separator;
+}
+
+function checkifForbidden(separator) {
+  for (const ch of separator) {
+    if (FORBIDDEN.includes(ch))
+      throw new Error(`[ERROR] ${ch} 는 커스텀 구분자로 사용할 수 없습니다.`);
+  }
+}
+
+function validateInput(str) {
+  const regex = new RegExp(`[^0-9,]`, "g");
+  for (const ch of str) {
+    if (regex.test(ch)) {
+      throw new Error(`[ERROR] '${ch}'는 지정하지 않은 구분자입니다.`);
+    }
+  }
+}
+
+function getNumArr(input) {
   const arr = input.split(",");
   const result = [];
   for (const num of arr) {
@@ -19,47 +68,9 @@ export function validateInput(input) {
   return result;
 }
 
-function removeSpace(input) {
-  return input.replaceAll(" ", "");
-}
-
-export function replaceSeparator(input) {
-  if (!input.includes("\\n")) {
-    return checkSeparator(input.replaceAll(":", ","));
-  }
-  let [separator, str] = input.split("\\n");
-  separator = getSeparator(separator);
-  separator += ":";
-  const regex = new RegExp(`[${separator}]`, "g");
-  return checkSeparator(str.replaceAll(regex, ","));
-}
-
-function getSeparator(seperator) {
-  if (!seperator.startsWith("//")) {
-    throw new Error(
-      `[ERROR] 커스텀 구분자를 사용하려면 앞에 "//"를 입력해주세요`
-    );
-  }
-  return validateSeparator(seperator.substr(2));
-}
-
-function validateSeparator(separator) {
-  for (const ch of separator) {
-    if (FORBIDDEN.includes(ch))
-      throw new Error(`[ERROR] ${ch} 는 커스텀 구분자로 사용할 수 없습니다.`);
-  }
-  return separator;
-}
-
-function checkSeparator(str) {
-  if (/[^0-9,:]/.test(str))
-    throw new Error(`[ERROR] 커스텀 구분자를 지정하세요`);
-  return str;
-}
-
 function convertToNumber(num) {
   if (Number(num)) return Number(num);
-  throw new Error(`[Error] ${num} 은 올바른 입력이 아닙니다.`);
+  throw new Error(`[ERROR] ${num} 은 올바른 입력이 아닙니다.`);
   //   const startsWith = /^\d/;
   //   const endsWith = /\d$/;
   //   if (!startsWith.test(num) || !endsWith.test(num))
